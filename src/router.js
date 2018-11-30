dd915824c00f88b0b9e9e46dc506a95d3be3658b
CRIMX <straybugs@gmail.com>
2018-11-30 13:37:00 +0800

    add ssr to generate static pages

--- src/router.js
+++ src/router.js
@@ -4,19 +4,21 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import(/* webpackChunkName: "about" */ './views/Projects.vue')
-    }
-  ]
-})
+export function createRouter () {
+  return new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: Home
+      },
+      {
+        path: '/projects',
+        name: 'projects',
+        component: () => import(/* webpackChunkName: "projects" */ './views/Projects.vue')
+      }
+    ]
+  })
+}
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,39 @@
+const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
+const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
+const nodeExternals = require('webpack-node-externals')
+
+const TARGET_NODE = process.env.WEBPACK_TARGET === 'node'
+
 module.exports = {
-  lintOnSave: false
+  configureWebpack: () => ({
+    entry: `./src/entry-${TARGET_NODE ? 'server' : 'client'}`,
+    target: TARGET_NODE ? 'node' : 'web',
+    node: TARGET_NODE ? undefined : false,
+    plugins: [
+      TARGET_NODE
+        ? new VueSSRServerPlugin()
+        : new VueSSRClientPlugin()
+    ],
+    externals: TARGET_NODE ? nodeExternals({
+      whitelist: /\.css$/
+    }) : undefined,
+    output: {
+      libraryTarget: TARGET_NODE
+        ? 'commonjs2'
+        : undefined
+    },
+    optimization: {
+      splitChunks: undefined
+    }
+  }),
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options =>
+        Object.assign(options, {
+          optimizeSSR: false
+        })
+      )
+  }
 }
