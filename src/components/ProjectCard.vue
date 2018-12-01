8d476ce6712f1339392bca0daf6828589ff76a56
CRIMX <straybugs@gmail.com>
2018-12-01 18:52:48 +0800

    Add project card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="projects-Card">
+    <header>
+      <h1 v-t="`projects.${id}.title`" />
+    </header>
+    <div>
+      <stack-icon
+        v-for="name in stack"
+        :key="name"
+        :name="stacks[name][0]"
+        :href="stacks[name][1]"
+      >{{ stacks[name][2] }}</stack-icon>
+    </div>
+    <i18n
+      v-for="(c, i) in $i18n.messages[$i18n.locale].projects[id].content"
+      :key="c"
+      :path="`projects.${id}.content.${i}`"
+       tag="p"
+     >
+      <slot />
+    </i18n>
+  </section>
+</template>
+
+<script>
+import StackIcon from '@/components/StackIcon'
+
+export default {
+  props: ['id', 'stack'],
+  data () {
+    return {
+      stacks: {
+        'vue': ['vue', 'https://github.com/vuejs/vue', 'Vue.js'],
+        'vue-router': ['vue', 'https://github.com/vuejs/vue-router', 'Vue-router'],
+        'vue-ssr': ['vue', 'https://ssr.vuejs.org/', 'Vue SSR'],
+        'vue-i18n': ['vue', 'https://github.com/kazupon/vue-i18n', 'Vue-i18n']
+      }
+    }
+  },
+  components: {
+    StackIcon
+  }
+}
+</script>
---
+++ src/components/StackIcon.vue
@@ -0,0 +1,28 @@
+<template functional>
+  <a class="stackicon-Wrap" :href="props.href">
+    <svg v-if="props.name === 'vue'" class="stackicon-Icon" preserveAspectRatio="xMidYMid" viewBox="0 18 257 221">
+      <path fill="#41B883" d="M205.3 18.1h51.2l-128 220.8L.5 18.1h97.92l30.08 51.2 29.44-51.2h47.36z"/>
+      <path fill="#41B883" d="M.5 18.1l128 220.8 128-220.8h-51.2l-76.8 132.48L51.06 18.1H.5z"/>
+      <path fill="#35495E" d="M51.06 18.1l77.44 133.12L205.3 18.1h-47.36L128.5 69.3 98.42 18.1H51.06z"/>
+    </svg>
+    <slot />
+  </a>
+</template>
+
+<style scoped lang="scss">
+.stackicon-Wrap {
+  text-decoration: none;
+  margin-right: 0.8em;
+  line-height: 1;
+  white-space: nowrap;
+  word-break: keep-all;
+  font-size: 14px;
+  color: inherit;
+}
+
+.stackicon-Icon {
+  max-width: 2em;
+  height: 0.8em;
+  margin-right: 2px;
+}
+</style>
