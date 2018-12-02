01beab1b00b901b6d88676c0a6efcf6c2501f98b
CRIMX <straybugs@gmail.com>
2018-12-02 19:32:10 +0800

    break IFC

--- src/components/StackIcon.vue
+++ src/components/StackIcon.vue
@@ -1,5 +1,5 @@
 <template functional>
-  <a class="stackicon-Wrap" :href="props.href">
+  <a class="stackicon-Wrap" :href="props.href" target="_blank" rel="nofollow">
     <svg v-if="props.name === 'vue'" class="stackicon-Icon" preserveAspectRatio="xMidYMid" viewBox="0 18 257 221">
       <path fill="#41B883" d="M205.3 18.1h51.2l-128 220.8L.5 18.1h97.92l30.08 51.2 29.44-51.2h47.36z"/>
       <path fill="#41B883" d="M.5 18.1l128 220.8 128-220.8h-51.2l-76.8 132.48L51.06 18.1H.5z"/>
@@ -11,6 +11,7 @@
 
 <style scoped lang="scss">
 .stackicon-Wrap {
+  display: inline-block;
   text-decoration: none;
   margin-right: 0.8em;
   line-height: 1;
