e57e227f69043f9e01ad4804b37ac72d243af6a8
CRIMX <straybugs@gmail.com>
2018-12-02 19:29:29 +0800

    Add normalize scss

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,14 @@
 </template>
 
 <style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+@import '~normalize-scss';
 
 html,
 body,
 #app {
   height: 100%;
+  margin: 0;
+  padding: 0;
 }
 
 #app {
